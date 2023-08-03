9ee362f0487d44dc0bb1daa6709b84d3e4d0607e
Vivian Rice <weixu1220@gmail.com>
2023-08-03 10:24:07 -0600

    restyle css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,9 +18,9 @@ function Home() {
     return (
         <div className="flex-col w-full">
         <div className="flex flex-wrap justify-center">
-            {localStorage.getItem('username') && <h2 className="text-3xl font-bold text-amber-400 text-center">Welcome to Rift Archive, {localStorage.getItem('username')}!</h2>}
-            {!localStorage.getItem('username') && <input className="border-2 rounded-md text-3xl p-1 m-1 text-center" type="text" value={username} onChange={handleChange} placeholder="username"/>}
-            {!localStorage.getItem('username') && <button className="border-2 rounded-md text-amber-400 text-xl text-center p-3 m-1 bg-blue-950" onClick={saveUsernameToLocal}>Save Username</button>}
+            {localStorage.getItem('username') && <h2 className="text-3xl font-bold text-amber-400 text-center mt-10">Welcome to Rift Archive, {localStorage.getItem('username')}!</h2>}
+            {!localStorage.getItem('username') && <input className="border-2 rounded-md text-3xl p-1 m-1 mt-10 text-center" type="text" value={username} onChange={handleChange} placeholder="username"/>}
+            {!localStorage.getItem('username') && <button className="border-2 rounded-md text-amber-400 text-xl text-center p-3 m-1 mt-10 bg-blue-950" onClick={saveUsernameToLocal}>Save Username</button>}
         </div>
             <img className="mx-auto my-5 map"src={summonersRiftMap} alt="Summoner's Rift Map" />
         </div>
